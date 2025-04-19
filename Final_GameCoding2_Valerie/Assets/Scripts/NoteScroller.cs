857d5c76a4088e5a4b04a2ecf7bc7f4a2fc40677
mantafsh <bullfinchhedge@gmail.com>
2025-04-19 01:00:05 -0400

    NOTES MOVING. FOREVER

--- Final_GameCoding2_Valerie/Assets/Scripts/NoteScroller.cs
+++ Final_GameCoding2_Valerie/Assets/Scripts/NoteScroller.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.TerrainTools;
 using UnityEngine;
 
 public class NoteScroller : MonoBehaviour
 {
+    public float noteSpeed;
+    public bool hasStarted;
     // Start is called before the first frame update
     void Start()
     {
-        
+        noteSpeed = Conductor.bpm / 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!hasStarted)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true;
+            }
+        }
+        else 
+        {
+            transform.position -= new Vector3(noteSpeed * Time.deltaTime, 0, 0); 
+        }
     }
 }
